8e7d86e8880d5ccc3301cab0610a1905de1c5d60
padsley <padsley@gmail.com>
2014-08-19 17:34:15 +0200

    Add new files HagarData.c and setup.sh - first to deal with Hagar sorting, the second to deal with setting up things automatically.

---
+++ setup.sh
@@ -0,0 +1,1 @@
+#This code will, ideally, become the setup script for running things, where you can set the detector types, the ADC channels etc. etc. etc. It currently does nothing because I haven't managed to make it yet.
