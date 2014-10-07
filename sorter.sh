f1a7392c4c8becdbb23b1fbdc57cd3cf2d25e815
padsley <padsley@gmail.com>
2014-10-07 14:39:54 +0200

    Minor change to Makefile to remove the reference to -lgsl -lgslcblas in the Makefile because arthur doesn't seem to have these.

--- sorter.sh
+++ sorter.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
 
-for COUNT in {1000..1101}
+for COUNT in {0..200}
 do
   echo $COUNT
-  FILE="/media/Elements/PR226/run0"$COUNT".mid.gz"
+  if [ $COUNT -lt 10 ]
+  then
+      FILE="/experiment-data/PR231/run0000"$COUNT".mid.gz"
+  elif [ $COUNT -lt 100 ]
+  then
+      FILE="/experiment-data/PR231/run000"$COUNT".mid.gz"
+  elif [ $COUNT -lt 1000 ]
+  then
+      FILE="/experiment-data/PR231/run00"$COUNT".mid.gz"
+  elif [ $COUNT -lt 10000 ]
+  then
+      FILE="/experiment-data/PR231/run0"$COUNT".mid.gz"
+  else
+      echo "This might not work"
+  fi
   echo $FILE
-  FILE2="/media/Elements/PR226/sorted0"$COUNT".root"
+  if [ $COUNT -lt 10 ]
+  then
+      FILE2="/k600user/PR231/data/sorted0000"$COUNT".root"
+  elif [ $COUNT -lt 100 ]
+  then
+      FILE2="/k600user/PR231/data/sorted000"$COUNT".root"
+  elif [ $COUNT -lt 1000 ]
+  then
+      FILE2="/k600user/PR231/data/sorted00"$COUNT".root"
+  elif [ $COUNT -lt 10000 ]
+  then
+      FILE2="/k600user/PR231/data/sorted0"$COUNT".root"
+  else
+      echo "This might not work"
+  fi
+  echo $FILE2
   if [ -e $FILE ]
   then
       if [ ! -e $FILE2 ]
 	  then
-	  ./analyzer -i /media/Elements/PR226/run0$COUNT.mid.gz
-	  mv output.root /media/Elements/PR226/sorted0$COUNT.root
+	  #./analyzer -i /k600user/PR231/data/run000"$COUNT".mid.gz
+	  ./analyzer -i $FILE
+	  #mv output.root /k600user/PR231/data/sorted000$COUNT.root
+	  mv output.root $FILE2
       fi
   fi
 done
