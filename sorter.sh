e0d76eab3818c551e5b938acfddb9875e4b07b42
padsley <padsley@gmail.com>
2015-03-05 12:02:04 +0200

    SetupDev update from laptop

--- sorter.sh
+++ sorter.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-for COUNT in {0..1000}
+for COUNT in {1046..1067}
 do
   echo $COUNT
   if [ $COUNT -lt 10 ]
   then
-      FILE="/experiment-data/PR231/run0000"$COUNT".mid.gz"
+      FILE="/media/Elements/PR227/run0000"$COUNT".mid.gz"
   elif [ $COUNT -lt 100 ]
   then
-      FILE="/experiment-data/PR231/run000"$COUNT".mid.gz"
+      FILE="/media/Elements/PR227/run000"$COUNT".mid.gz"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE="/experiment-data/PR231/run00"$COUNT".mid.gz"
+      FILE="/media/Elements/PR227/run00"$COUNT".mid.gz"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE="/experiment-data/PR231/run0"$COUNT".mid.gz"
+      FILE="/media/Elements/PR227/run0"$COUNT".mid.gz"
   else
       echo "This might not work"
   fi
   echo $FILE
   if [ $COUNT -lt 10 ]
   then
-      FILE2="/k600user/PR231/data/sorted0000"$COUNT".root"
+      FILE2="/media/Elements/PR227/sorted0000"$COUNT".root"
   elif [ $COUNT -lt 100 ]
   then
-      FILE2="/k600user/PR231/data/sorted000"$COUNT".root"
+      FILE2="/media/Elements/PR227/sorted000"$COUNT".root"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE2="/k600user/PR231/data/sorted00"$COUNT".root"
+      FILE2="/media/Elements/PR227/sorted00"$COUNT".root"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE2="/k600user/PR231/data/sorted0"$COUNT".root"
+      FILE2="/media/Elements/PR227/sorted0"$COUNT".root"
   else
       echo "This might not work"
   fi
@@ -40,10 +40,10 @@ do
       if [ ! -e $FILE2 ]
 	  then
 	  echo $FILE
-	  ./analyzer -i /experiment-data/PR231/run000"$COUNT".mid.gz
-	  #./analyzer -i $FILE
-	  mv output.root /k600user/PR231/data/sorted000$COUNT.root
-	  #mv output.root $FILE2
+# 	  ./analyzer -i /media/Elements/PR227/run000"$COUNT".mid.gz
+	  ./analyzer -i $FILE
+# 	  mv output.root /media/Elements/PR227/sorted000$COUNT.root
+	  mv output.root $FILE2
       fi
   fi
 done
