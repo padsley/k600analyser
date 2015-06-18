f6d589daf2e57e9e6e2585ec75c7c943257b4036
padsley <padsley@gmail.com>
2015-06-18 13:18:42 +0200

    Changes from laptop - just to avoid merge conflicts later

--- sorter.sh
+++ sorter.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-for COUNT in {80..1045}
+for COUNT in {1089..1101}
 do
   echo $COUNT
   if [ $COUNT -lt 10 ]
   then
-      FILE="/afs/tlabs.ac.za/user/p/padsley/data/PR227/run0000"$COUNT".mid.gz"
+      FILE="/media/Elements/PR226/run0000"$COUNT".mid.gz"
   elif [ $COUNT -lt 100 ]
   then
-      FILE="/afs/tlabs.ac.za/user/p/padsley/data/PR227/run000"$COUNT".mid.gz"
+      FILE="/media/Elements/PR226/run000"$COUNT".mid.gz"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE="/afs/tlabs.ac.za/user/p/padsley/data/PR227/run00"$COUNT".mid.gz"
+      FILE="/media/Elements/PR226/run00"$COUNT".mid.gz"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE="/afs/tlabs.ac.za/user/p/padsley/data/PR227/run0"$COUNT".mid.gz"
+      FILE="/media/Elements/PR226/run0"$COUNT".mid.gz"
   else
       echo "This might not work"
   fi
   echo $FILE
   if [ $COUNT -lt 10 ]
   then
-      FILE2="/afs/tlabs.ac.za/user/p/padsley/data/PR227/sorted0000"$COUNT".root"
+      FILE2="/media/Elements/PR226/sorted0000"$COUNT".root"
   elif [ $COUNT -lt 100 ]
   then
-      FILE2="/afs/tlabs.ac.za/user/p/padsley/data/PR227/sorted000"$COUNT".root"
+      FILE2="/media/Elements/PR226/sorted000"$COUNT".root"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE2="/afs/tlabs.ac.za/user/p/padsley/data/PR227/sorted00"$COUNT".root"
+      FILE2="/media/Elements/PR226/sorted00"$COUNT".root"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE2="/afs/tlabs.ac.za/user/p/padsley/data/PR227/sorted0"$COUNT".root"
+      FILE2="/media/Elements/PR226/sorted0"$COUNT".root"
   else
       echo "This might not work"
   fi
@@ -40,9 +40,9 @@ do
       if [ ! -e $FILE2 ]
       then
       echo $FILE
-#       ./analyzer -i /afs/tlabs.ac.za/user/p/padsley/data/PR227/run000"$COUNT".mid.gz
+#       ./analyzer -i /media/Elements/PR226/run000"$COUNT".mid.gz
       ./analyzer -i $FILE
-#       mv output.root /afs/tlabs.ac.za/user/p/padsley/data/PR227/sorted000$COUNT.root
+#       mv output.root /media/Elements/PR226/sorted000$COUNT.root
       mv output.root $FILE2
       fi
   fi
