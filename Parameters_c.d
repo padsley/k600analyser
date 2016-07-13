46e0d5350bde1a43ecc04047553b5d155a6872e4
padsley <padsley@gmail.com>
2016-07-13 12:57:21 +0200

    Push disabling of the MMMThetaCalc until fixed

--- Parameters_c.d
+++ Parameters_c.d
@@ -2,5 +2,5 @@
 # DO NOT DELETE
 
 ./Parameters_c.so: Parameters.h
-./Parameters_c.so: /home/luna/root/include/cintdictversion.h /home/luna/root/include/RVersion.h
-Parameters_c__ROOTBUILDVERSION= 5.34/37
+./Parameters_c.so: /home/padsley/codes/cern_root/root-v5-34/include/cintdictversion.h /home/padsley/codes/cern_root/root-v5-34/include/RVersion.h
+Parameters_c__ROOTBUILDVERSION= 5.34/14
