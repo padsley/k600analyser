141344837d8b9ea6ffcf93c313c1828777875d8f
LindsD <lindsay.donaldson18@gmail.com>
2017-01-17 10:44:07 +0200

    Adjusted file directories

--- sorter.sh
+++ sorter.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 
-for COUNT in {01069,01070,01071,01072,01076,01077,01078,01082,01084,01085,01087,01088,01090,01091,01092,01093,01098,01099,01100}
 
+for COUNT in {1069,1070,1071,1072,1076,1077,1078,1082,1084,1085,1087,1088,1090,1091,1092,1093,1098,1099,1100}
+#for COUNT in {1069,1070}
 
 do
   echo $COUNT
   if [ $COUNT -lt 10 ]
   then
-      FILE="/media/lindsay/Elements/Backups/Linux/DATA2/MID/run0000"$COUNT".mid"
+      FILE="/media/lindsay/Elements/PR217/run0000"$COUNT".mid.gz"
   elif [ $COUNT -lt 100 ]
   then
-      FILE="/media/lindsay/Elements/Backups/Linux/DATA2/MID/run000"$COUNT".mid"
+      FILE="/media/lindsay/Elements/PR217/run000"$COUNT".mid.gz"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE="/media/lindsay/Elements/Backups/Linux/DATA2/MID/run00"$COUNT".mid"
+      FILE="/media/lindsay/Elements/PR217/run00"$COUNT".mid.gz"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE="/media/lindsay/Elements/Backups/Linux/DATA2/MID/run0"$COUNT".mid"
+      FILE="/media/lindsay/Elements/PR217/run0"$COUNT".mid.gz"
   elif [ $COUNT -lt 100000 ]
   then
-      FILE="/media/lindsay/Elements/Backups/Linux/DATA2/MID/run"$COUNT".mid"
+      FILE="/media/lindsay/Elements/PR217/run"$COUNT".mid.gz"
   else
       echo "This might not work"
   fi
