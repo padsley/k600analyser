2fb77560f33c525c48bd45025453fa0b5cf5af7a
Retief Neveling <neveling@tlabs.ac.za>
2017-01-25 11:37:39 +0200

    corrected sorter.sh, it does not like ~

--- sorter.sh
+++ sorter.sh
@@ -5,53 +5,56 @@ for COUNT in {1108..1109}
 
 
 do
-  echo $COUNT
+  echo "inside DO loop, with runnr = " $COUNT
   if [ $COUNT -lt 10 ]
   then
-      FILE="~/DATA/MID/PR236/run0000"$COUNT".mid.gz"
+      FILE="/home/neveling/DATA/MID/PR236/run0000"$COUNT".mid.gz"
   elif [ $COUNT -lt 100 ]
   then
-      FILE="~/DATA/MID/PR236/run000"$COUNT".mid.gz"
+      FILE="/home/neveling/DATA/MID/PR236/run000"$COUNT".mid.gz"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE="~/DATA/MID/PR236/run00"$COUNT".mid.gz"
+      FILE="/home/neveling/DATA/MID/PR236/run00"$COUNT".mid.gz"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE="~/DATA/MID/PR236/run0"$COUNT".mid.gz"
+      FILE="/home/neveling/DATA/MID/PR236/run0"$COUNT".mid.gz"
   elif [ $COUNT -lt 100000 ]
   then
-      FILE="~/DATA/MID/PR236/run"$COUNT".mid.gz"
+      FILE="/home/neveling/DATA/MID/PR236/run"$COUNT".mid.gz"
   else
       echo "This might not work"
   fi
-  echo $FILE
+  echo "midas file name: " $FILE
 
   if [ $COUNT -lt 10 ]
   then
-      FILE2="~/DATA/ROOT/PR260/sorted0000"$COUNT".root"
+      FILE2="/home/neveling/DATA/ROOT/PR260/sorted0000"$COUNT".root"
   elif [ $COUNT -lt 100 ]
   then
-      FILE2="~/DATA/ROOT/PR260/sorted000"$COUNT".root"
+      FILE2="/home/neveling/DATA/ROOT/PR260/sorted000"$COUNT".root"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE2="~/DATA/ROOT/PR260/sorted00"$COUNT".root"
+      FILE2="/home/neveling/DATA/ROOT/PR260/sorted00"$COUNT".root"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE2="~/DATA/ROOT/PR260/sorted0"$COUNT".root"
+      FILE2="/home/neveling/DATA/ROOT/PR260/sorted0"$COUNT".root"
   elif [ $COUNT -lt 100000 ]
   then
-      FILE2="~/DATA/ROOT/PR260/sorted"$COUNT".root"
+      FILE2="/home/neveling/DATA/ROOT/PR260/sorted"$COUNT".root"
   else
       echo "This might not work"
   fi
-  echo $FILE2
+  echo "and desired rootfile name : " $FILE2
 
   if [ -e $FILE ]
   then
+      echo "entered IF loop to start analyzer"
+
       if [ ! -e $FILE2 ]
       then
       echo $FILE
 #       ./analyzer -i /media/Elements/PR226/run000"$COUNT".mid.gz
+      echo "./analyzer -i "$FILE
       ./analyzer -i $FILE
 #       mv output.root /media/Elements/PR226/sorted000$COUNT.root
       mv output.root $FILE2
