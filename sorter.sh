5e601ce74bc9e028c96a2ccfd6d2697e985c0f34
Retief Neveling <neveling@tlabs.ac.za>
2017-01-25 10:40:43 +0200

    new sorter for pr236

--- sorter.sh
+++ sorter.sh
@@ -1,49 +1,51 @@
 #!/bin/bash
 
 
-for COUNT in {10350..10351}
+for COUNT in {1108..1109}
 
 
 do
   echo $COUNT
   if [ $COUNT -lt 10 ]
   then
-      FILE="/experiment-data/PR238/2016/run0000"$COUNT".mid.gz"
+      FILE="~/DATA/MID/PR236/run0000"$COUNT".mid.gz"
   elif [ $COUNT -lt 100 ]
   then
-      FILE="/experiment-data/PR238/2016/run000"$COUNT".mid.gz"
+      FILE="~/DATA/MID/PR236/run000"$COUNT".mid.gz"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE="/experiment-data/PR238/2016/run00"$COUNT".mid.gz"
+      FILE="~/DATA/MID/PR236/run00"$COUNT".mid.gz"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE="/experiment-data/PR238/2016/run0"$COUNT".mid.gz"
+      FILE="~/DATA/MID/PR236/run0"$COUNT".mid.gz"
   elif [ $COUNT -lt 100000 ]
   then
-      FILE="/experiment-data/PR238/2016/run"$COUNT".mid.gz"
+      FILE="~/DATA/MID/PR236/run"$COUNT".mid.gz"
   else
       echo "This might not work"
   fi
   echo $FILE
+
   if [ $COUNT -lt 10 ]
   then
-      FILE2="/vicepda/althome/k600user/PR238_2016/sortedRuns/sorted0000"$COUNT".root"
+      FILE2="~/DATA/ROOT/PR260/sorted0000"$COUNT".root"
   elif [ $COUNT -lt 100 ]
   then
-      FILE2="/vicepda/althome/k600user/PR238_2016/sortedRuns/sorted000"$COUNT".root"
+      FILE2="~/DATA/ROOT/PR260/sorted000"$COUNT".root"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE2="/vicepda/althome/k600user/PR238_2016/sortedRuns/sorted00"$COUNT".root"
+      FILE2="~/DATA/ROOT/PR260/sorted00"$COUNT".root"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE2="/vicepda/althome/k600user/PR238_2016/sortedRuns/sorted0"$COUNT".root"
+      FILE2="~/DATA/ROOT/PR260/sorted0"$COUNT".root"
   elif [ $COUNT -lt 100000 ]
   then
-      FILE2="/vicepda/althome/k600user/PR238_2016/sortedRuns/sorted"$COUNT".root"
+      FILE2="~/DATA/ROOT/PR260/sorted"$COUNT".root"
   else
       echo "This might not work"
   fi
   echo $FILE2
+
   if [ -e $FILE ]
   then
       if [ ! -e $FILE2 ]
