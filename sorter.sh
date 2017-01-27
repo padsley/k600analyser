48ec8a12dc7471fef165c9bd1ae70ff66211595e
Retief Neveling <neveling@tlabs.ac.za>
2017-01-27 16:21:08 +0200

    dir structure correct for Sunday

--- sorter.sh
+++ sorter.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
 
-#for COUNT in {2125..2134}
-#for COUNT in {2125,2126,2130,2134}
-
-for COUNT in {1093,1100,1104,1108,1112,1117,1123,1128}
+for COUNT in {1093..1128}
 
 
 do
   echo "inside DO loop, with runnr = " $COUNT
   if [ $COUNT -lt 10 ]
   then
-      FILE="/home/neveling/DATA/MID/PR236/run0000"$COUNT".mid.gz"
+      FILE="/home/sunday/DATA/MID/PR236/run0000"$COUNT".mid.gz"
   elif [ $COUNT -lt 100 ]
   then
-      FILE="/home/neveling/DATA/MID/PR236/run000"$COUNT".mid.gz"
+      FILE="/home/sunday/DATA/MID/PR236/run000"$COUNT".mid.gz"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE="/home/neveling/DATA/MID/PR236/run00"$COUNT".mid.gz"
+      FILE="/home/sunday/DATA/MID/PR236/run00"$COUNT".mid.gz"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE="/home/neveling/DATA/MID/PR236/run0"$COUNT".mid.gz"
+      FILE="/home/sunday/DATA/MID/PR236/run0"$COUNT".mid.gz"
   elif [ $COUNT -lt 100000 ]
   then
-      FILE="/home/neveling/DATA/MID/PR236/run"$COUNT".mid.gz"
+      FILE="/home/sunday/DATA/MID/PR236/run"$COUNT".mid.gz"
   else
       echo "This might not work"
   fi
@@ -31,19 +28,19 @@ do
 
   if [ $COUNT -lt 10 ]
   then
-      FILE2="/home/neveling/DATA/ROOT/PR236/sorted0000"$COUNT".root"
+      FILE2="/home/sunday/DATA/ROOT/PR236/sorted0000"$COUNT".root"
   elif [ $COUNT -lt 100 ]
   then
-      FILE2="/home/neveling/DATA/ROOT/PR236/sorted000"$COUNT".root"
+      FILE2="/home/sunday/DATA/ROOT/PR236/sorted000"$COUNT".root"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE2="/home/neveling/DATA/ROOT/PR236/sorted00"$COUNT".root"
+      FILE2="/home/sunday/DATA/ROOT/PR236/sorted00"$COUNT".root"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE2="/home/neveling/DATA/ROOT/PR236/sorted0"$COUNT".root"
+      FILE2="/home/sunday/DATA/ROOT/PR236/sorted0"$COUNT".root"
   elif [ $COUNT -lt 100000 ]
   then
-      FILE2="/home/neveling/DATA/ROOT/PR236/sorted"$COUNT".root"
+      FILE2="/home/sunday/DATA/ROOT/PR236/sorted"$COUNT".root"
   else
       echo "This might not work"
   fi
