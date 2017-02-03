00f714013d434877b956230373fcfb194b42f049
LindsD <lindsay.donaldson18@gmail.com>
2017-02-03 15:17:09 +0200

    Defined FULLANALYSIS

--- main.c
+++ main.c
@@ -55,7 +55,7 @@
 // #define _JJAUTOTRIM
 // #define _PRINTTOSCREEN
 // #define _VDCRESCALCS
-// #define _FULLANALYSIS
+ #define _FULLANALYSIS
 // #define _MISALIGNTIME
 // #define _RAWDATA
 // #define _SILICONDATA 
@@ -1721,10 +1721,10 @@ INT main_event(EVENT_HEADER * pheader, void *pevent)
    // Now calculate and fill spectra for calculated angles using 2 driftchambers, and calculate Ypos
    // Note that if X1flag==0 then the event passed all gates: pid, dt, group. It is for good events only
    //--------------------------------------------------------------------------------------------------------
-   thetaFP = CalcThetaFP(X1pos,X2pos);
-   t_thetaFP = thetaFP;
-   //thetaFP  = CalcThetaFP(U1pos,U2pos);
-   //t_thetaFP   = thetaFP;
+   //thetaFP = CalcThetaFP(X1pos,X2pos);
+   //t_thetaFP = thetaFP;
+   thetaFP  = CalcThetaFP(U1pos,U2pos);
+   t_thetaFP   = thetaFP;
 
    Y1=CalcYFP(X1pos,U1pos,X1th);  
    t_Y1=Y1;
