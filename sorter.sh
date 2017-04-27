6804bba8ed51724865a701d7269b107d02c4b69a
KevinCWLi <kcwli@sun.ac.za>
2017-04-27 16:20:13 +0200

    Updated run numbers for PR240. The data and sorted directories are now entered with string variables to allow quicker directory changes.

--- sorter.sh
+++ sorter.sh
@@ -1,60 +1,87 @@
 #!/bin/bash
 
-#for COUNT in {1069,1070,1071,1072,1076,1077,1078,1082,1084,1085,1087,1088,1090,1091,1092,1093,1098,1099,1100}
-for COUNT in {1084,1087,1088,1092,1093}
+#================================
+#	PR240 - preWE1 (CAKE Calibration)
+#for COUNT in {108,109,110,111}
+
+#================================
+#	PR240 - WE1
+#for COUNT in {479,484,488,490,492,496,499,502,505,539,543,549,551,553,554,557,561}
+
+#================================
+#	PR240 - WE2
+for COUNT in {1016,1020,1023,1027,1029,1033,1035,1037,1038,1054,1058,1067,1070,1074,1081,1082,1084,1090,1091,1093,1095,1097,1098,1104,1124,1127,1137,1157,1160,1186,1189,1206,1208,1244,1256,1258,1262,1265,1270,1278,1280,1287}
+
+#================================
+#	PR240 - WE1 && WE2
+#for COUNT in {479,484,488,490,492,496,499,502,505,539,543,549,551,553,554,557,561,1016,1020,1023,1027,1029,1033,1035,1037,1038,1054,1058,1067,1070,1074,1081,1082,1084,1090,1091,1093,1095,1097,1098,1104,1124,1127,1137,1157,1160,1186,1189,1206,1208,1244,1256,1258,1262,1265,1270,1278,1280,1287}
+
+#================================
+#	PR240 - preWE1 && WE1 && WE2
+#for COUNT in {108,109,110,111,479,484,488,490,492,496,499,502,505,539,543,549,551,553,554,557,561,1016,1020,1023,1027,1029,1033,1035,1037,1038,1054,1058,1067,1070,1074,1081,1082,1084,1090,1091,1093,1095,1097,1098,1104,1124,1127,1137,1157,1160,1186,1189,1206,1208,1244,1256,1258,1262,1265,1270,1278,1280,1287}
+
 
 do
+
+#====================================================
+#       Directories for the data and sorted data
+dataDirectory="/media/psf/Home/Academic/Experiments/PR240/data"
+sortedDirectory="/media/psf/Home/Academic/Experiments/PR240/sorted"
+
+
   echo $COUNT
   if [ $COUNT -lt 10 ]
   then
-      FILE="/experiment-data/PR251/2016/run0000"$COUNT".mid.gz"
+      FILE=$dataDirectory"/run0000"$COUNT".mid.gz"
   elif [ $COUNT -lt 100 ]
   then
-      FILE="/experiment-data/PR251/2016/run000"$COUNT".mid.gz"
+      FILE=$dataDirectory"/run000"$COUNT".mid.gz"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE="/experiment-data/PR251/2016/run00"$COUNT".mid.gz"
+      FILE=$dataDirectory"/run00"$COUNT".mid.gz"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE="/experiment-data/PR251/2016/run0"$COUNT".mid.gz"
+      FILE=$dataDirectory"/run0"$COUNT".mid.gz"
   elif [ $COUNT -lt 100000 ]
   then
-      FILE="/experiment-data/PR251/2016/run"$COUNT".mid.gz"
+      FILE=$dataDirectory"/run"$COUNT".mid.gz"
   else
       echo "This might not work"
   fi
   echo $FILE
   if [ $COUNT -lt 10 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted0000"$COUNT".root"
-      FILE2="../sortedfiles/sorted0000"$COUNT".root"
+      FILE2=$sortedDirectory"/sorted0000"$COUNT".root"
   elif [ $COUNT -lt 100 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted000"$COUNT".root"
-      FILE2="../sortedfiles/sorted000"$COUNT".root"
+      FILE2=$sortedDirectory"/sorted000"$COUNT".root"
   elif [ $COUNT -lt 1000 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted00"$COUNT".root"
-      FILE2="../sortedfiles/sorted00"$COUNT".root"
+      FILE2=$sortedDirectory"/sorted00"$COUNT".root"
   elif [ $COUNT -lt 10000 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted0"$COUNT".root"
-      FILE2="../sortedfiles/sorted0"$COUNT".root"
+      FILE2=$sortedDirectory"/sorted0"$COUNT".root"
   elif [ $COUNT -lt 100000 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted"$COUNT".root"
-      FILE2="../sortedfiles/sorted"$COUNT".root"
+      FILE2=$sortedDirectory"/sorted"$COUNT".root"
   else
       echo "This might not work"
   fi
   echo $FILE2
   if [ -e $FILE ]
   then
-      if [ ! -e $FILE2 ]
-      then
+  
       echo $FILE
       ./analyzer -i $FILE
       mv output.root $FILE2
-      fi
+      
+      
+#      if [ ! -e $FILE2 ]
+#      then
+#      echo $FILE
+#      ./analyzer -i $FILE
+#      mv output.root $FILE2
+#      cp output.root $FILE2
+#      fi
   fi
 done
