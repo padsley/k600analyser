e12f5a90f3fd28b8f692d98301a6f0beaca243f4
Alexander Long <along4@nd.edu>
2017-05-01 12:11:35 -0600

    Changed the config.cfg to reflect the PR242 setup

---
+++ Parameters_c.d
@@ -0,0 +1,6 @@
+
+# DO NOT DELETE
+
+./Parameters_c.so: Parameters.h
+./Parameters_c.so: /opt/64bit/root/root_v5.34.05/include/cintdictversion.h /opt/64bit/root/root_v5.34.05/include/RVersion.h
+Parameters_c__ROOTBUILDVERSION= 5.34/05
