e671ba507825b8d9387f32cbea08b7a32c7ca654
LindsD <lindsay.donaldson18@gmail.com>
2017-05-10 10:16:17 +0200

    Changes for analysis check

--- sorter.sh
+++ sorter.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 
-for COUNT in {1093..1128}
+for COUNT in {1128,3133,3151}
 
 
 do
   echo "inside DO loop, with runnr = " $COUNT
   if [ $COUNT -lt 10 ]
   then
-      FILE="/home/sunday/DATA/MID/PR236/run0000"$COUNT".mid.gz"
+      FILE="/media/lmdonaldson/Elements/PR236/run0000"$COUNT".mid.gz"
   elif [ $COUNT -lt 100 ]
   then
-      FILE="/home/sunday/DATA/MID/PR236/run000"$COUNT".mid.gz"
+      FILE="/media/lmdonaldson/Elements/PR236/run000"$COUNT".mid.gz"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE="/home/sunday/DATA/MID/PR236/run00"$COUNT".mid.gz"
+      FILE="/media/lmdonaldson/Elements/PR236/run00"$COUNT".mid.gz"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE="/home/sunday/DATA/MID/PR236/run0"$COUNT".mid.gz"
+      FILE="/media/lmdonaldson/Elements/PR236/run0"$COUNT".mid.gz"
   elif [ $COUNT -lt 100000 ]
   then
-      FILE="/home/sunday/DATA/MID/PR236/run"$COUNT".mid.gz"
+      FILE="/media/lmdonaldson/Elements/PR236/run"$COUNT".mid.gz"
   else
       echo "This might not work"
   fi
@@ -28,19 +28,19 @@ do
 
   if [ $COUNT -lt 10 ]
   then
-      FILE2="/home/sunday/DATA/ROOT/PR236/sorted0000"$COUNT".root"
+      FILE2="/home/lmdonaldson/K600/DATA/PR236/sorted0000"$COUNT".root"
   elif [ $COUNT -lt 100 ]
   then
-      FILE2="/home/sunday/DATA/ROOT/PR236/sorted000"$COUNT".root"
+      FILE2="/home/lmdonaldson/K600/DATA/PR236/sorted000"$COUNT".root"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE2="/home/sunday/DATA/ROOT/PR236/sorted00"$COUNT".root"
+      FILE2="/home/lmdonaldson/K600/DATA/PR236/sorted00"$COUNT".root"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE2="/home/sunday/DATA/ROOT/PR236/sorted0"$COUNT".root"
+      FILE2="/home/lmdonaldson/K600/DATA/PR236/sorted0"$COUNT".root"
   elif [ $COUNT -lt 100000 ]
   then
-      FILE2="/home/sunday/DATA/ROOT/PR236/sorted"$COUNT".root"
+      FILE2="/home/lmdonaldson/K600/DATA/PR236/sorted"$COUNT".root"
   else
       echo "This might not work"
   fi
