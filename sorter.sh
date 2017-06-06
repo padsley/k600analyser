5b43711a827928a15a1c689455913262112f7a4f
Lyna10 <baloyi.Lynah@gmail.com>
2017-06-06 12:49:40 +0200

    Changed sorter for L. Baloyi case

--- sorter.sh
+++ sorter.sh
@@ -1,49 +1,44 @@
 #!/bin/bash
 
 #for COUNT in {1069,1070,1071,1072,1076,1077,1078,1082,1084,1085,1087,1088,1090,1091,1092,1093,1098,1099,1100}
-for COUNT in {1084,1087,1088,1092,1093}
+for COUNT in {2142,2143}
 
 do
   echo $COUNT
   if [ $COUNT -lt 10 ]
   then
-      FILE="/experiment-data/PR251/2016/run0000"$COUNT".mid.gz"
+      FILE="/home/lbaloyi/K600/Data/PR275/MID/run0000"$COUNT".mid.gz"
   elif [ $COUNT -lt 100 ]
   then
-      FILE="/experiment-data/PR251/2016/run000"$COUNT".mid.gz"
+      FILE="/home/lbaloyi/K600/Data/PR275/MID/run000"$COUNT".mid.gz"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE="/experiment-data/PR251/2016/run00"$COUNT".mid.gz"
+      FILE="/home/lbaloyi/K600/Data/PR275/MID/run00"$COUNT".mid.gz"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE="/experiment-data/PR251/2016/run0"$COUNT".mid.gz"
+      FILE="/home/lbaloyi/K600/Data/PR275/MID/run0"$COUNT".mid.gz"
   elif [ $COUNT -lt 100000 ]
   then
-      FILE="/experiment-data/PR251/2016/run"$COUNT".mid.gz"
+      FILE="/home/lbaloyi/K600/Data/PR275/MID/run"$COUNT".mid.gz"
   else
       echo "This might not work"
   fi
   echo $FILE
   if [ $COUNT -lt 10 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted0000"$COUNT".root"
-      FILE2="../sortedfiles/sorted0000"$COUNT".root"
+      FILE2="/home/lbaloyi/K600/Data/PR275/ROOT/sorted0000"$COUNT".root"
   elif [ $COUNT -lt 100 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted000"$COUNT".root"
-      FILE2="../sortedfiles/sorted000"$COUNT".root"
+      FILE2="/home/lbaloyi/K600/Data/PR275/ROOT/sorted000"$COUNT".root"
   elif [ $COUNT -lt 1000 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted00"$COUNT".root"
-      FILE2="../sortedfiles/sorted00"$COUNT".root"
+      FILE2="/home/lbaloyi/K600/Data/PR275/ROOT/sorted00"$COUNT".root"
   elif [ $COUNT -lt 10000 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted0"$COUNT".root"
-      FILE2="../sortedfiles/sorted0"$COUNT".root"
+      FILE2="/home/lbaloyi/K600/Data/PR275/ROOT/sorted0"$COUNT".root"
   elif [ $COUNT -lt 100000 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted"$COUNT".root"
-      FILE2="../sortedfiles/sorted"$COUNT".root"
+      FILE2="/home/lbaloyi/K600/Data/PR275/ROOT/sorted"$COUNT".root"
   else
       echo "This might not work"
   fi
