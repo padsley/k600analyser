ecd63a64045678643fa8ddd548d60e643a9deb0b
Chane13M <chane13.m@gmail.com>
2017-06-20 09:49:13 +0200

    Changed location where DriftLength spectra are filled to inside raytrace

--- main.c
+++ main.c
@@ -1449,22 +1449,22 @@ INT main_event(EVENT_HEADER * pheader, void *pevent)
    for(int i = 0; i < X1hits_dt ; i++) { 
      drifttime=X1.time[i];
      X1.dist[i]=lutx1[drifttime]*DRIFTLENGTH;     //X1.dist is driftlength in actual mm
-     hX1_DriftLength->Fill(X1.dist[i]);
+     //hX1_DriftLength->Fill(X1.dist[i]);         //Decide to fill these histograms within the raytrace algorithm
    }
    for(int i = 0; i < X2hits_dt ; i++) { 
      drifttime=X2.time[i];
      X2.dist[i]=lutx2[drifttime]*DRIFTLENGTH;
-     hX2_DriftLength->Fill(X2.dist[i]);
+     //hX2_DriftLength->Fill(X2.dist[i]);
    }
    for(int i = 0; i < U1hits_dt ; i++) { 
      drifttime=U1.time[i];
      U1.dist[i]=lutu1[drifttime]*DRIFTLENGTH;
-     hU1_DriftLength->Fill(U1.dist[i]);
+     //hU1_DriftLength->Fill(U1.dist[i]);
    }
    for(int i = 0; i < U2hits_dt ; i++) { 
      drifttime=U2.time[i];
      U2.dist[i]=lutu2[drifttime]*DRIFTLENGTH;
-     hU2_DriftLength->Fill(U2.dist[i]);
+     //hU2_DriftLength->Fill(U2.dist[i]);
    }
    
    //printf("min x wires %i,  max x wires %i \n",globals.min_x_wires, globals.max_x_wires);
@@ -1525,7 +1525,8 @@ INT main_event(EVENT_HEADER * pheader, void *pevent)
 	   hEventID->Fill(ev_id_X1_good);  // good X1 events 
 	   for(int i = 0; i < X1hits_dt ; i++) { 
 		//if(X1pos>0 && X1pos<249){
-	     hX1_DriftTimeGood->Fill(X1.time[i]);	
+	     hX1_DriftTimeGood->Fill(X1.time[i]);
+	     hX1_DriftLength->Fill(fabs(X1.dist[i]));	
                 //}
 	   }
 	   #ifdef _FULLANALYSIS
@@ -1586,7 +1587,8 @@ INT main_event(EVENT_HEADER * pheader, void *pevent)
 	   hU1_EffID->Fill(ev_good);
 	   hEventID->Fill(ev_id_U1_good);  // good U1 events 
 	   for(int i = 0; i < U1hits_dt ; i++) { 
-	     hU1_DriftTimeGood->Fill(U1.time[i]);	
+	     hU1_DriftTimeGood->Fill(U1.time[i]);
+	     hU1_DriftLength->Fill(fabs(U1.dist[i]));	
 	   }
 	   #ifdef _FULLANALYSIS
 	   hU1_Chisq->Fill(U1chisq);
@@ -1644,7 +1646,8 @@ INT main_event(EVENT_HEADER * pheader, void *pevent)
 	   hX2_EffID->Fill(ev_good);
 	   hEventID->Fill(ev_id_X2_good);  // good X2 events 
 	   for(int i = 0; i < X2hits_dt ; i++) { 
-	     hX2_DriftTimeGood->Fill(X2.time[i]);	
+	     hX2_DriftTimeGood->Fill(X2.time[i]);
+	     hX2_DriftLength->Fill(fabs(X2.dist[i]));	
 	   }
 	   #ifdef _FULLANALYSIS
 	   hX2_Chisq->Fill(X2chisq);
@@ -1703,7 +1706,8 @@ INT main_event(EVENT_HEADER * pheader, void *pevent)
 	   hU2_EffID->Fill(ev_good);
 	   hEventID->Fill(ev_id_U2_good);  // good U2 events 	 
 	   for(int i = 0; i < U2hits_dt ; i++) { 
-	     hU2_DriftTimeGood->Fill(U2.time[i]);	
+	     hU2_DriftTimeGood->Fill(U2.time[i]);
+	     hU2_DriftLength->Fill(fabs(U2.dist[i]));	
 	   }
 	   #ifdef _FULLANALYSIS
 	   hU2_Chisq->Fill(U2chisq);
