5685de5da6c9e7b22174b5b60f3d6e9bdbc28965
Retief Neveling <neveling@tlabs.ac.za>
2019-10-08 15:15:55 +0200

    updates for PR236

--- sorter.sh
+++ sorter.sh
@@ -1,53 +1,56 @@
 #!/bin/bash
 
-#for COUNT in {1069,1070,1071,1072,1076,1077,1078,1082,1084,1085,1087,1088,1090,1091,1092,1093,1098,1099,1100}
-for COUNT in {1084,1087,1088,1092,1093}
+#PR236b, 0 degr, 42Ca
+for COUNT in {177,178,180}
+
 
 do
   echo $COUNT
   if [ $COUNT -lt 10 ]
   then
-      FILE="/experiment-data/PR251/2016/run0000"$COUNT".mid.gz"
+      FILE="/home/neveling/DATA/MID/PR236b/run0000"$COUNT".mid.gz"
+      #FILE="/run/media/neveling/Transcend/DATA/MID/PR236b/run0000"$COUNT".mid.gz"
   elif [ $COUNT -lt 100 ]
   then
-      FILE="/experiment-data/PR251/2016/run000"$COUNT".mid.gz"
+      FILE="/home/neveling/DATA/MID/PR236b/run000"$COUNT".mid.gz"
+      #FILE="/run/media/neveling/Transcend/DATA/MID/PR236b/run000"$COUNT".mid.gz"
   elif [ $COUNT -lt 1000 ]
   then
-      FILE="/experiment-data/PR251/2016/run00"$COUNT".mid.gz"
+      FILE="/home/neveling/DATA/MID/PR236b/run00"$COUNT".mid.gz"
+      #FILE="/run/media/neveling/Transcend/DATA/MID/PR236b/run00"$COUNT".mid.gz"
   elif [ $COUNT -lt 10000 ]
   then
-      FILE="/experiment-data/PR251/2016/run0"$COUNT".mid.gz"
+      FILE="/home/neveling/DATA/MID/PR236b/run0"$COUNT".mid.gz"
+      #FILE="/run/media/neveling/Transcend/DATA/MID/PR236b/run0"$COUNT".mid.gz"
   elif [ $COUNT -lt 100000 ]
   then
-      FILE="/experiment-data/PR251/2016/run"$COUNT".mid.gz"
+      FILE="/home/neveling/DATA/MID/PR236b/run"$COUNT".mid.gz"
+      #FILE="/run/media/neveling/Transcend/DATA/MID/PR236b/run"$COUNT".mid.gz"
   else
       echo "This might not work"
   fi
   echo $FILE
+
   if [ $COUNT -lt 10 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted0000"$COUNT".root"
-      FILE2="../sortedfiles/sorted0000"$COUNT".root"
+      FILE2="/home/neveling/DATA/ROOT/PR236b/sorted0000"$COUNT".root"
   elif [ $COUNT -lt 100 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted000"$COUNT".root"
-      FILE2="../sortedfiles/sorted000"$COUNT".root"
+      FILE2="/home/neveling/DATA/ROOT/PR236b/sorted000"$COUNT".root"
   elif [ $COUNT -lt 1000 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted00"$COUNT".root"
-      FILE2="../sortedfiles/sorted00"$COUNT".root"
+      FILE2="/home/neveling/DATA/ROOT/PR236b/sorted00"$COUNT".root"
   elif [ $COUNT -lt 10000 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted0"$COUNT".root"
-      FILE2="../sortedfiles/sorted0"$COUNT".root"
+      FILE2="/home/neveling/DATA/ROOT/PR236b/sorted0"$COUNT".root"
   elif [ $COUNT -lt 100000 ]
   then
-#      FILE2="/vicepda/althome/k600user/PR251/2016/sortedfiles/sorted"$COUNT".root"
-      FILE2="../sortedfiles/sorted"$COUNT".root"
+      FILE2="/home/neveling/DATA/ROOT/PR236b/sorted"$COUNT".root"
   else
       echo "This might not work"
   fi
   echo $FILE2
+
   if [ -e $FILE ]
   then
       if [ ! -e $FILE2 ]
@@ -58,3 +61,13 @@ do
       fi
   fi
 done
+
+
+
+
+
+
+
+
+
+
