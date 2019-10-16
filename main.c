802c4fcef964cac0e2777ad339608dcec907bb14
Retief <neveling@tlabs.ac.za>
2019-10-16 03:23:07 +0200

    changed ThFP calc

--- main.c
+++ main.c
@@ -1779,9 +1779,9 @@ INT main_event(EVENT_HEADER * pheader, void *pevent)
    // Now calculate and fill spectra for calculated angles using 2 driftchambers, and calculate Ypos
    // Note that if X1flag==0 then the event passed all gates: pid, dt, group. It is for good events only
    //--------------------------------------------------------------------------------------------------------
-   //thetaFP = CalcThetaFP(X1pos,X2pos);
+   thetaFP = CalcThetaFP(X1pos,X2pos);
    //t_thetaFP = thetaFP;
-   thetaFP  = CalcThetaFP(U1pos,U2pos);
+   //thetaFP  = CalcThetaFP(U1pos,U2pos);
    t_thetaFP   = thetaFP;
 
    Y1=CalcYFP(X1pos,U1pos,X1th);  
