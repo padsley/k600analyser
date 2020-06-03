7e893c41bf0f8408c60a0b5c4c7bc030bc2d13d2
LindsD <lindsay.donaldson18@gmail.com>
2020-06-03 17:46:53 +0200

    Initial PR292 analysis and starting files

---
+++ CUT_pad1vstof_run4030.C
@@ -0,0 +1,19 @@
+{
+//========= Macro generated from object: CUT_pad1vstof_run4030/Graph
+//========= by ROOT version5.34/36
+   
+   TCutG *cutg = new TCutG("CUT_pad1vstof_run4030",8);
+   cutg->SetVarX("tof");
+   cutg->SetVarY("pad1");
+   cutg->SetTitle("Graph");
+   cutg->SetFillColor(1);
+   cutg->SetPoint(0,2255.95,625.265);
+   cutg->SetPoint(1,2248.42,954.352);
+   cutg->SetPoint(2,2216.19,1071.13);
+   cutg->SetPoint(3,2189.33,1071.13);
+   cutg->SetPoint(4,2187.18,869.427);
+   cutg->SetPoint(5,2202.22,646.497);
+   cutg->SetPoint(6,2240.9,508.493);
+   cutg->SetPoint(7,2255.95,625.265);
+   cutg->Draw("");
+}
---
+++ CUT_pad1vstof_run4032.C
@@ -0,0 +1,18 @@
+{
+//========= Macro generated from object: CUT_pad1vstof_run4032/Graph
+//========= by ROOT version5.34/36
+   
+   TCutG *cutg = new TCutG("CUT_pad1vstof_run4032",7);
+   cutg->SetVarX("tof");
+   cutg->SetVarY("pad1");
+   cutg->SetTitle("Graph");
+   cutg->SetFillColor(1);
+   cutg->SetPoint(0,2257.02,975.584);
+   cutg->SetPoint(1,2203.3,1177.28);
+   cutg->SetPoint(2,2180.73,837.58);
+   cutg->SetPoint(3,2210.82,582.803);
+   cutg->SetPoint(4,2262.39,487.261);
+   cutg->SetPoint(5,2262.39,933.121);
+   cutg->SetPoint(6,2257.02,975.584);
+   cutg->Draw("");
+}
